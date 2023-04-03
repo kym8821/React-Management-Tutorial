5cf97626674474d65fafef9425c051c5814afb9e
kym8821 <louie9798@naver.com>
2023-04-03 22:07:02 +0900

    Create React Project

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Getting Started with Create React App
+## Management System
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+본 프로젝트는 React 연습을 위해 만든 고객 관리 시스템입니다.
+(Management System)입니다.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+//웹사이트의 내용을 출력하는 친구
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grey-background">
+      <img src={logo} lat='logo'/>
+      <h2>Let's develop management system!</h2>
     </div>
   );
 }
